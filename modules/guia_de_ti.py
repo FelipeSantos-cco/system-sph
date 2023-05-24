83e346736bdb7244e6b68aafaf72a0423d54af64
unknown <felipe.almeida@sptech.school>
2023-05-24 17:45:32 -0300

    Busca de plataformas

--- modules/guia_de_ti.py
+++ modules/guia_de_ti.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 
 class Buscar():
 
-    global listaLinks
-
     def pesquisa_plataforma():
 
         url = 'https://guiadeti.com.br/'
@@ -15,6 +13,9 @@ class Buscar():
             'Chrome/51.0.2704.103 Safari/537.36'
         }
 
+        listaLinks = []
+        listaPlatLink = []
+
         resposta = requests.get(url, headers= header)
 
         if resposta.status_code >= 200 and resposta.status_code < 300:
@@ -22,21 +23,34 @@ class Buscar():
             print("Buscando plataformas...")
             
             site = BeautifulSoup(resposta.content, 'html.parser')
-            tabelaCursos = site.find('div', attrs={'class': 'fusion-fullwidth fullwidth-box fusion-builder-row-4 fusion-flex-container nonhundred-percent-fullwidth non-hundred-percent-height-scrolling'})
+            divLinks = site.find('div', attrs={'class': 'fusion-fullwidth fullwidth-box fusion-builder-row-4 fusion-flex-container nonhundred-percent-fullwidth non-hundred-percent-height-scrolling'})
 
-            listaLinks = []
+            for link in divLinks.find_all('a'):
 
-            for link in tabelaCursos.find_all('a'):
                 if link.get('href') not in listaLinks: # Não pegar links repetidos
                     listaLinks.append(link.get('href'))
 
             #print(listaLinks)
             for i in range(0, len(listaLinks)):
-                print(f"Entrando no Link: {listaLinks[i]}")
                 resposta = requests.get(listaLinks[i], headers= header)
                 
                 if resposta.status_code >= 200 and resposta.status_code < 300:
+                    print(f"Entrando no Link: {listaLinks[i]}")
+
                     site = BeautifulSoup(resposta.content, 'html.parser')
+                    divPlataformas = site.find('div', attrs={'class': 'fusion-portfolio-wrapper'})
+
+                    for link in divPlataformas.find_all('a'):
+
+                        if link.get('href') not in listaPlatLink:
+                            listaPlatLink.append(link.get('href'))
+
+
+            for i in range(0, len(listaPlatLink)):
+                print(f"Plataforma: {listaPlatLink[i]}")
+                            
+                            
+                         
 
         else:
             print(f"Erro na conexão\nResposta: {resposta.status_code}")
