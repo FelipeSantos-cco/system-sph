95b175f73508106d8b293d5da58ecc6693f99f4c
unknown <felipe.almeida@sptech.school>
2023-05-23 22:45:05 -0300

    Teste de POO

---
+++ main.py
@@ -0,0 +1,8 @@
+import modules.buscadores
+
+lib = modules.buscadores
+
+if __name__ == '__main__':
+    
+    lib.pesquisaGoogle("teste")
+
---
+++ modules/buscadores.py
@@ -0,0 +1,4 @@
+print("Vc fez a chamada da API Google.py")
+
+def pesquisaGoogle(query):
+    print(f"vc está pesquisando por {query}")
