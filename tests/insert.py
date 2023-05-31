66548458a2bc89a7fbd1db350962dccb4563dc3d
unknown <felipe.almeida@sptech.school>
2023-05-31 20:59:43 -0300

    Inserção automática das plataformas: OK

--- tests/insert.py
+++ tests/insert.py
@@ -1,27 +1,50 @@
 import requests #, os
+import mysql.connector
 from bs4 import BeautifulSoup
 
-# print(os.getcwd())
+def database(nomePlat, descPlat, urlPlat, urlImg):
+    meubd = mysql.connector.connect(
+        host="localhost",
+        user="aluno",
+        password="sptech",
+        database="bdsphack"
+    )
 
-arquivo = open('../plataformas.txt')
-links = arquivo.readlines()
-arquivo.close()
+    mycursor = meubd.cursor()
 
-for link in links:
-    resposta = requests.get(link)
+    sql = "INSERT INTO bdsphack.tbplataforma (nomePlataforma, descPlataforma, urlPlataforma, urlImgPlataforma) VALUES(%s, %s, %s, %s);"
+    val = (nomePlat, descPlat, urlPlat, urlImg)
 
-    if resposta.status_code >= 200 and resposta.status_code < 300:
-        site = BeautifulSoup(resposta.content, 'html.parser')
+    mycursor.execute(sql, val)
+    meubd.commit()
 
-        titulo = site.find('h1', attrs={'class': 'entry-title'}).contents[0]
+    return print(f'Inseriu: {nomePlat}\n') 
 
-        descricao = 0#site.find()
-        
-        urlImg = site.find('img', attrs={'decoding': 'async'})
 
-        urlPlataforma = site.find('div', attrs={'class': 'project-info'}).find('a', attrs={'target' : '_blank'}).get('href')
+def buscaLink():
 
-        print(f'{titulo}\n{descricao}\n{urlPlataforma}\n{urlImg}')
+    arquivo = open('../plataformas.txt')
+    links = arquivo.readlines()
+    arquivo.close()
 
-    if True:
-        break
+    for link in links:
+        resposta = requests.get(link)
+
+        if resposta.status_code >= 200 and resposta.status_code < 300:
+            site = BeautifulSoup(resposta.content, 'html.parser')
+
+            titulo = site.find('h1', attrs={'class': 'entry-title'}).contents[0]
+
+            descricao = site.find('div', attrs={'class': 'fusion-text fusion-text-1 fusion-text-no-margin'}).get_text()
+
+            urlImg = site.find('img', attrs={'decoding': 'async'}).get('data-src')
+            urlImg = 'https://guiadeti.com.br'+urlImg
+
+            urlPlataforma = site.find('div', attrs={'class': 'project-info'}).find('a', attrs={'target' : '_blank'}).get('href')
+
+            # print(f'{titulo}\n{descricao}\n{urlPlataforma}\n{urlImg}')
+            database(titulo, descricao, urlPlataforma, urlImg)
+
+
+
+buscaLink()
